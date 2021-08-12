1306b446bf1305826e70ae906a55247e3c2da8c1
Wolfgang Roese <mail@wolfgangroese.de>
2021-08-12 20:20:00 +0200

    End of Section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,5 @@
+import { MembersService } from './../../_services/members.service';
+import { Member } from './../../_modules/member';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  members: Member[];
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    })
   }
 
 }
